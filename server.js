55720246aa98425300eb7d0318fd74bf30e53405
DemianHallyfax <d.erizocopanco3@gmail.com>
2023-10-06 07:17:42 -0600

    Cambios en los archivos. Ademas de borrar los que consideraba 'redundantes', por lo que se ha reducido (un poco) el tamaño total del proyecto. Y se han realizado comentarios y cambios en el README.md para saber que más hacer a futuro.

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require("express"),
   app = express(),
-  storiesRouter = require('./routes/routerStories'),
+  storiesRouter = require("./routes/routerStories"),
   articleRouter = require("./routes/router");
 
+//---------------------------------------------
+//MIDLEWARE
+//---------------------------------------------
 app.set("view engine", "ejs");
 
+//---------------------------------------------
+//ROUTERS
+//---------------------------------------------
 app.use(articleRouter);
 app.use(storiesRouter);
 
+//---------------------------------------------
+//EXTRAS
+//---------------------------------------------
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static("public"));
 
 app.listen(5000);
- 
