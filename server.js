3498de88b5178a3059816a78cc84ecfb3e78bb52
DemianHallyfax <d.erizocopanco3@gmail.com>
2023-10-09 07:05:05 -0600

    Comienzo de la creacion del sistema de Paginacion.(Roguemos a los Dioses que todo salga bien).

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express"),
   app = express(),
   storiesRouter = require("./routes/routerStories"),
-  articleRouter = require("./routes/router");
+  articleRouter = require("./routes/router"),
+  paginationRouter = require("./routes/pagination");
 
 //---------------------------------------------
 //MIDLEWARE
@@ -13,11 +14,13 @@ app.set("view engine", "ejs");
 //---------------------------------------------
 app.use(articleRouter);
 app.use(storiesRouter);
+app.use(paginationRouter);
 
 //---------------------------------------------
 //EXTRAS
 //---------------------------------------------
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static("public"));
+
 
 app.listen(5000);
