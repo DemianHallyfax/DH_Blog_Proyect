bd41be4464752dd78c9ce32eb27b4442a2806d0f
DemianHallyfax <d.erizocopanco3@gmail.com>
2024-01-25 07:13:14 -0600

    Avance en el SGDH y en la gestion de Articulos

--- routes/routerSGDH.js
+++ routes/routerSGDH.js
@@ -1,6 +1,7 @@
 const express = require('express'),
     app = express();
-    routerSGDH = express.Router();
+    routerSGDH = express.Router(),
+    articles = require("../info_json/articlesTEST.json");
 
 app.use(express.static(__dirname + '/public'));
 app.use('/public', express.static('public'));
@@ -8,5 +9,9 @@ app.use('/public', express.static('public'));
 routerSGDH.get('/SGDH/lobby', (req, res) => {
     res.render('SGDH');
 });
+
+routerSGDH.get('/SGDH/articles', (req, res) => {
+    res.render('SGDH/articles', articles);
+})
 
 module.exports = routerSGDH;
