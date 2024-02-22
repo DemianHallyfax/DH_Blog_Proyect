f687262848a4c0f710a4deeeff1633aa5f2c6246
DemianHallyfax <d.erizocopanco3@gmail.com>
2024-02-22 06:52:57 -0600

    Accesar de forma remota con ip

--- server.js
+++ server.js
@@ -3,7 +3,30 @@ const express = require("express"),
   storiesRouter = require("./routes/routerStories"),
   articleRouter = require("./routes/router"),
   routerGallery = require("./routes/routerGallery"),
-  PORT = 5000;
+  PORT = 5000,
+  os = require('os');
+
+  const nets = os.networkInterfaces();
+  const results = {}; // Or just '{}', an empty object
+  
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+      const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+      if (net.family === familyV4Value && !net.internal) {
+        if (!results[name]) {
+          results[name] = [];
+        }
+        results[name].push(net.address);
+      }
+    }
+  }
+
+  let IP = results.Ethernet[0];
+
+  console.log('Conectar de forma local a:');
+  console.log(`http://${IP}:${PORT}/`);
 
 /*---------------------------------------------
 MIDLEWARE
