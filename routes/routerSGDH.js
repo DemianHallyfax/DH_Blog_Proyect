f4adb22ebe68b666ec782604fb33f7c68bc25e87
DemianHallyfax <d.erizocopanco3@gmail.com>
2024-03-27 06:39:40 -0600

    Optimizando CSS I

--- routes/routerSGDH.js
+++ routes/routerSGDH.js
@@ -1,68 +1,74 @@
-const express = require('express'),
-    app = express();
-    routerSGDH = express.Router(),
-    Article = require("./../models/article");
+const express = require("express"),
+  app = express();
+(routerSGDH = express.Router()), (Article = require("./../models/article"));
 
-app.use(express.static(__dirname + '/public'));
-app.use('/public', express.static('public'));
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static("public"));
 
-routerSGDH.get('/SGDH/lobby', (req, res) => {
-    res.render('SGDH');
+routerSGDH.get("/SGDH/lobby", (req, res) => {
+  res.render("SGDH");
 });
 /* MOSTRAR / CREAR ARTICULOS --------------------*/
-routerSGDH.get('/SGDH/articles', async (req, res) => {
-    const articles = await Article.find().sort({ createdAt: 'desc'});
-    res.render('SGDH/articles', { articles });
+routerSGDH.get("/SGDH/articles", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("SGDH/articles", { articles });
 });
 
-routerSGDH.get('/SGDH/articles/new', (req, res) => {
-    res.render('SGDH/new', {article: new Article() });
+routerSGDH.get("/SGDH/articles/new", (req, res) => {
+  res.render("SGDH/new", { article: new Article() });
 });
 
-routerSGDH.post('/SGDH/articles', async (req, res, next) => {
+routerSGDH.post(
+  "/SGDH/articles",
+  async (req, res, next) => {
     req.article = new Article();
     next();
-}, saveArticleAndRedirect('new'));
+  },
+  saveArticleAndRedirect("new")
+);
 
-routerSGDH.get('/SGDH/articles/:slug', async (req, res) => {
-    const article = await Article.findOne({ slug: req.params.slug });
-    if (article == null) res.redirect('/SGDH/articles');
-    res.render('blog-articles/show', { article: article });
+routerSGDH.get("/SGDH/articles/:slug", async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug });
+  if (article == null) res.redirect("/SGDH/articles");
+  res.render("blog-articles/show", { article: article });
 });
-
 
 /* EDITAR ARTICULOS ------------------------------*/
-routerSGDH.get('/SGDH/articles/edit/:id', async (req, res) => {
-    const article = await Article.findById(req.params.id);
-    res.render('SGDH/edit', {article: article });
+routerSGDH.get("/SGDH/articles/edit/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render("SGDH/edit", { article: article });
 });
 
-routerSGDH.put('/SGDH/articles/:id', async (req, res, next) => {
+routerSGDH.put(
+  "/SGDH/articles/:id",
+  async (req, res, next) => {
     req.article = await Article.findById(req.params.id);
     next();
-}, saveArticleAndRedirect('edit'));
+  },
+  saveArticleAndRedirect("edit")
+);
 
 /* BORRAR ARTICULOS -------------------------------*/
-routerSGDH.delete('/SGDH/articles/:id', async (req, res) => {
-    await Article.findByIdAndDelete(req.params.id);
-    res.redirect('/SGDH/articles');
+routerSGDH.delete("/SGDH/articles/:id", async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id);
+  res.redirect("/SGDH/articles");
 });
 
 function saveArticleAndRedirect(path) {
-    return async (req, res) => {
-        let article = req.article;
-            article.title = req.body.title,
-            article.img = req.body.img,
-            article.imgTemp = req.body.imgTemp,
-            article.description = req.body.description,
-            article.article = req.body.article
-        try{
-            article = await article.save();
-            res.redirect(`/SGDH/articles/${article.slug}`)
-        } catch (e){
-            res.render(`/SGDH/articles/${path}`, { article: article })
-        }
+  return async (req, res) => {
+    let article = req.article;
+    (article.title = req.body.title),
+      (article.img = req.body.img),
+      (article.imgTemp = req.body.imgTemp),
+      (article.description = req.body.description),
+      (article.article = req.body.article);
+    try {
+      article = await article.save();
+      res.redirect(`/SGDH/articles/${article.slug}`);
+    } catch (e) {
+      res.render(`/SGDH/articles/${path}`, { article: article });
     }
+  };
 }
 
 module.exports = routerSGDH;
--- server.js
+++ server.js
@@ -4,41 +4,45 @@ const express = require("express"),
   storiesRouter = require("./routes/routerStories"),
   articleRouter = require("./routes/router"),
   routerGallery = require("./routes/routerGallery"),
-  routerSGDH = require('./routes/routerSGDH'),
+  routerSGDH = require("./routes/routerSGDH"),
   PORT = 5000,
-  mongoose = require('mongoose');
+  mongoose = require("mongoose");
 
-  const os = require('os')
+/*---------------------------------------------*/
+//OBTENER IP PARA ACCEDER ATRAVEZ DE OTROS DISPOSITIVOS
+/*---------------------------------------------*/
+const os = require("os");
+const nets = os.networkInterfaces();
+const results = {}; // Or just '{}', an empty object
 
-  const nets = os.networkInterfaces();
-  const results = {}; // Or just '{}', an empty object
-  
-  for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-      // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-      const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
-      if (net.family === familyV4Value && !net.internal) {
-        if (!results[name]) {
-          results[name] = [];
-        }
-        results[name].push(net.address);
+for (const name of Object.keys(nets)) {
+  for (const net of nets[name]) {
+    // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+    // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+    const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
+    if (net.family === familyV4Value && !net.internal) {
+      if (!results[name]) {
+        results[name] = [];
       }
+      results[name].push(net.address);
     }
   }
+}
 
-  const IP = results.Ethernet[0];
-  console.log(`http://${IP}:${PORT}/`);
+const IP = results.Ethernet[0];
+console.log(`http://${IP}:${PORT}/`);
+/*---------------------------------------------*/
+/*---------------------------------------------*/
 
 /*---------------------------------------------
 MIDLEWARE
 ---------------------------------------------*/
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 try {
-  mongoose.connect('mongodb://127.0.0.1:27017/Demian_Hallyfax_Blog');
-  console.log('Data base connected.');
+  mongoose.connect("mongodb://127.0.0.1:27017/Demian_Hallyfax_Blog");
+  console.log("Data base connected.");
 } catch (error) {
   console.log(error);
 }
